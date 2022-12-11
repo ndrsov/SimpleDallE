4e5661c0b38c030c1f49cace87bc34ce16e6fb73
Andres Soria <andres.zov@gmail.com>
2022-12-11 13:29:22 -0600

    Setup post request to test it functions

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use("/openai", require("./routes/openAIRoutes"));
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
